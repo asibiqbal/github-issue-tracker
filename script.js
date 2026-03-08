b97cd8dc67651098927b0c7eaef0fadd9be790f1
asibiqbal <mohammadasibiqbal@gmail.com>
2026-03-08 19:23:33 +0600

    Added Login Section

---
+++ script.js
@@ -0,0 +1,18 @@
+// get login button
+document.getElementById('login-btn').addEventListener("click" , function(){
+  const inputUser = document.getElementById('input-user')
+  const inputPassword = document.getElementById('input-password')
+  inputPassword.innerText = ""
+
+  if (inputUser.value == "admin" && inputPassword.value == "admin123"){
+    alert('login successful');
+    window.location.assign("./home.html");
+  }
+  else{
+    alert('Login Failed');
+    return;
+  }
+  
+  inputUser.innerText = ""
+
+})
