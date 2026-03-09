5e4a72cdf31343b494e2cc103decbb277662bbe6
asibiqbal <mohammadasibiqbal@gmail.com>
2026-03-09 22:05:00 +0600

    added modal

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const btnSearch = document.getElementById('btn-search');
 // global variable to store all issues
 let allData = []; 
 
+// get modal
+const getModal = document.getElementById('get-modal')
+const modalContent = document.getElementById("modal-content");
+
 // toggle and active btn 
 function activeBtn(activeBtn) {
     [btnAll, btnOpen, btnClose].forEach(btn => {
@@ -74,7 +78,7 @@ function displayAll(allTabs) {
         }
 
         allCard.innerHTML = `
-            <div class="h-full py-6 px-5 border-x border-b border-t-[4px] ${borderTop} w-full border-gray-200 rounded-lg">
+            <div onclick="openModal(${allTab.id})" class="h-full py-6 px-5 border-x border-b border-t-[4px] ${borderTop} w-full border-gray-200 rounded-lg">
                 <div class="flex justify-between items-center">
                     <div>
                         ${allTab.status === "open"
@@ -100,9 +104,11 @@ function displayAll(allTabs) {
                         <img src="assets/dot.png" alt="" /> HELP WANTED
                     </div>
                 </div>
-                <div class="border-t border-gray-200 p-0 space-y-2 mt-5 pt-3">
+                <div class="border-t border-gray-200 p-0 space-y-1 mt-5 pt-5">
                     <p class="text-[#64748B]">#1 by ${allTab.author || 'User'}</p>
-                    <p class="text-[#64748B]">${allTab.createdAt || '1/15/2024'}</p>
+                    <p class="text-[#64748B]">
+${allTab.createdAt ? allTab.createdAt.split("T")[0] : '2024-01-15'}
+</p>
                 </div>
             </div>
         `;
@@ -142,5 +148,70 @@ btnClose.addEventListener('click', () => {
     loadAll('close');
 });
 
+// open Modal
+async function openModal(id){
+
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data = await res.json();
+
+    const issue = data.data;
+
+    const statusBadge =
+        issue.status === "open"
+            ? `<div class="badge badge-success rounded-full text-white">Opened</div>`
+            : `<div class="badge badge-info bg-purple-700 rounded-full text-white">Closed</div>`;
+
+    const formattedDate = new Date(issue.updatedAt).toLocaleDateString("en-GB");
+
+    modalContent.innerHTML = `
+        <h3 class="text-lg font-bold">${issue.title}</h3>
+
+        <div class="space-x-1 my-2">
+            <span>${statusBadge}</span>
+
+            <span class="font-extrabold text-gray-400 text-sm">.</span>
+
+            <span class="text-gray-400 text-sm">
+                Opened by ${issue.assignee}
+            </span>
+
+            <span class="font-extrabold text-gray-400 text-sm">.</span>
+
+            <span class="text-gray-400 text-sm">
+                ${formattedDate}
+            </span>
+        </div>
+
+        <p class="py-2 text-[#64748B]">
+            ${issue.description}
+        </p>
+
+        <div class="flex justify-between items-center bg-gray-100 p-4 rounded">
+            <div class="space-y-1">
+                <h5 class="text-[#64748B]">Assignee:</h5>
+                <p class="font-semibold">${issue.assignee}</p>
+            </div>
+
+            <div class="space-y-1">
+                <h5 class="text-[#64748B]">Priority:</h5>
+                <div class="badge border-none rounded-full text-white bg-red-600">
+                    ${issue.priority.toUpperCase()}
+                </div>
+            </div>
+        </div>
+
+        <div class="modal-action mt-6">
+            <form method="dialog">
+                <button class="btn btn-primary">Close</button>
+            </form>
+        </div>
+    `;
+
+    getModal.showModal();
+}
+
 // load all
 loadAll();
+
+
+
